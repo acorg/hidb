7cb2ffe3bb9ca301c22f24843e7d1cfc560874b2
Eugene Skepner <github@skepner.eu>
2016-11-07 10:48:54 +0000

    night-build fixed

--- python/hidb/utility.py
+++ python/hidb/utility.py
@@ -1,3 +1,4 @@
+import sys
 import logging; module_logger = logging.getLogger(__name__)
 from pathlib import Path
 import subprocess, tempfile, shutil, datetime
@@ -30,7 +31,7 @@ sAcmacsSuffixes = {".acd1", ".acp1", ".acmacs-txt"}
 def get_ace(temp_dir :Path, source1 :Path):
     if set(source1.suffixes) & sAcmacsSuffixes:
         source2 = temp_dir.joinpath(source1.stem + ".ace")
-        subprocess.run("env ACMACS_ROOT=$HOME/ac/acmacs PYTHONPATH=$HOME/ac/acmacs:$HOME/ac/acmacs/build $HOME/c2r/bin/python3 $HOME/ac/acmacs/bin/convert.py -q '{}' '{}'".format(source1, source2), shell=True, check=True)
+        subprocess.run("{acmacs_env} python3 $HOME/ac/acmacs/bin/convert.py -q '{source1}' '{source2}'".format(acmacs_env=acmacs_env, source1=source1, source2=source2), shell=True, check=True)
     elif source1.suffixes[-1] == ".ace":
         source2 = source1
     else:
@@ -42,13 +43,22 @@ def get_ace(temp_dir :Path, source1 :Path):
 
 def get_ace_data(source :Path):
     if set(source.suffixes) & sAcmacsSuffixes:
-        data = subprocess.run("env ACMACS_ROOT=$HOME/ac/acmacs PYTHONPATH=$HOME/ac/acmacs:$HOME/ac/acmacs/build $HOME/c2r/bin/python3 $HOME/ac/acmacs/bin/convert.py -q -f ace '{}' -".format(source), shell=True, stdout=subprocess.PIPE, check=True).stdout
+        data = subprocess.run("{acmacs_env} python3 $HOME/ac/acmacs/bin/convert.py -q -f ace '{source}' -".format(acmacs_env=acmacs_env(), source=source), shell=True, stdout=subprocess.PIPE, check=True).stdout
     elif source.suffixes[-1] == ".ace":
         data = open(str(source), "rb").read()
     else:
         data = None
         module_logger.warning('Unsupported suffix in {}'.format(source1))
     return data
+
+# ----------------------------------------------------------------------
+
+def acmacs_env():
+    if sys.platform == "linux":
+        LD_LIBRARY_PATH = "LD_LIBRARY_PATH=$HOME/c2r/lib:$HOME/ac/acmacs/build/backend"
+    else:
+        LD_LIBRARY_PATH = ""
+    return "env PATH=\"$HOME/c2r/bin:$PATH\" ACMACS_ROOT=$HOME/ac/acmacs PYTHONPATH=$HOME/ac/acmacs:$HOME/ac/acmacs/build " + LD_LIBRARY_PATH
 
 # ----------------------------------------------------------------------
 ### Local Variables:
