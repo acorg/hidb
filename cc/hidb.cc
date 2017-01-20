7bd62fcd7278b4e08bcc45325abd9d5857a5a66a
Eugene Skepner <github@skepner.eu>
2017-01-20 10:29:04 +0100

    find homologous sera for an antigen

--- cc/hidb.cc
+++ cc/hidb.cc
@@ -508,11 +508,9 @@ std::vector<const SerumData*> HiDb::find_homologous_sera(const AntigenData& aAnt
     std::vector<const SerumData*> result;
     const std::string antigen_name = aAntigen.data().name();
     const std::string antigen_variant_id = aAntigen.data().variant_id();
-    std::cerr << antigen_name << " vi:" << antigen_variant_id << std::endl;
     for (const auto& serum: sera()) {
         if (serum.data().name() == antigen_name && serum.has_homologous_variant_id(antigen_variant_id)) {
             result.push_back(&serum);
-            std::cerr << "  " << serum.data().full_name() << std::endl;
         }
     }
     return result;
