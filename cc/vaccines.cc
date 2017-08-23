585d4dca96b562448b6842af626a6bbacb90080e
Eugene Skepner <github@skepner.eu>
2017-08-23 16:53:59 +0200

    disable PARIS B/Vic vaccine, not used anymore by Crick

--- cc/vaccines.cc
+++ cc/vaccines.cc
@@ -32,7 +32,7 @@ static std::map<std::string, std::vector<hidb::Vaccine>> sVaccines = {
     {"BVICTORIA", {
             {"MALAYSIA/2506/2004",       hidb::Vaccine::Previous},
             {"BRISBANE/60/2008",         hidb::Vaccine::Current},
-            {"PARIS/1762/2009",          hidb::Vaccine::Current},
+              // {"PARIS/1762/2009",          hidb::Vaccine::Current}, // not used by Crick anymore, B/Ireland/3154/2016 is used instead (2017-08-21)
             {"SOUTH AUSTRALIA/81/2012",  hidb::Vaccine::Surrogate},
             {"IRELAND/3154/2016",  hidb::Vaccine::Surrogate},
         }},
